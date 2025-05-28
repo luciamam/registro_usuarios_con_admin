7e942278b9c223fda5cf934c76008b59ed8a5e4b
luciamam <lumanmba2023@gmail.com>
2025-05-28 23:55:34 +0200

    insertando en la base de datos al usuario, e iniciando la sesion , funcionando correnctamente, mongodb

--- src/app.py
+++ src/app.py
@@ -1,17 +1,25 @@
-from flask import Flask, redirect,render_template
+from flask import Flask, redirect,render_template,request,redirect,url_for,flash
 from flask_bootstrap import Bootstrap4
 from formularios.forms import RegisterForm,LoginForm
 from dotenv import load_dotenv
+from werkzeug.security import check_password_hash,generate_password_hash
+from datetime import datetime,UTC
 
 load_dotenv()
 import os
+#  ahora vamos a introduci mongodb 
 
+from pymongo import MongoClient
 
 
 
 app=Flask(__name__)
 Bootstrap4(app)
 app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
+client = MongoClient("mongodb://localhost:27017/")
+db=client['registros_usuarios_con_admin']
+usuarios=db['usuarios']
+
 
 
 
@@ -27,6 +35,26 @@ def mostrar_register():
     return render_template('Register.html',form=form)
 
 
+@app.route('/register',methods=['POST'])
+def registrarse():
+    datos=request.form
+    name=datos['name']
+    email=datos['email']
+    password=datos['password']
+    usuario={
+            "name":name,
+            "email":email,
+            "password":generate_password_hash(password),
+            "fecha":datetime.now(UTC)
+            }
+            # estamos insertando aqui el usuario 
+    db.usuarios.insert_one(usuario)
+    return redirect(url_for('perfil'))
+    
+
+
+
+
 
 
 
@@ -35,6 +63,33 @@ def mostrar_login():
     form=LoginForm()
     
     return render_template('Login.html',form=form)
+
+
+@app.route('/login',methods=['POST'])
+def iniciar_sesion():
+    datos=request.form
+    email=datos['email']
+    password=datos['password']
+
+    usuario=db.usuarios.find_one({"email":email})
+    print("el usuario",usuario)
+    if usuario:
+        if check_password_hash(usuario['password'],password):
+            return redirect(url_for('perfil'))
+        else:
+            flash("contraseña incorrecta","info")
+            return  redirect(url_for('mostrar_login'))
+
+    else:
+        flash("usario no existe ","danger")
+        return  redirect(url_for('mostrar_login'))
+
+
+
+
+@app.route('/perfil')
+def perfil():
+    return render_template('Perfil.html')
 
 
 @app.errorhandler(404)
--- src/formularios/forms.py
+++ src/formularios/forms.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired,Length, EqualTo ,Email
 
 class RegisterForm(FlaskForm):
     name=StringField('Introduce tu nombre', validators=[DataRequired(),Length(min=4,max=12)])
-    email=EmailField('introduce un correo electronico',validators=[DataRequired(),Length(min=4,max=12),Email()])
+    email=EmailField('introduce un correo electronico',validators=[DataRequired(),Length(min=4,max=400),Email()])
     password=PasswordField('Introduce una contraseña', validators=[DataRequired(),Length(min=4,max=12),EqualTo('confirm')])
     confirm=PasswordField('Introduce una contraseña', validators=[DataRequired(),Length(min=4,max=12)])
     submit=SubmitField('Registrarse')
@@ -15,7 +15,7 @@ class RegisterForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email=EmailField('introduce un correo electronico',validators=[DataRequired(),Length(min=4,max=12),Email()])
+    email=EmailField('introduce un correo electronico',validators=[DataRequired(),Length(min=4,max=400),Email()])
     password=PasswordField('Introduce una contraseña', validators=[DataRequired(),Length(min=4,max=12)])
     submit=SubmitField('iniciar Sesion ')
     
